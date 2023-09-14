28ea4c8884038107a4860d43e2563fad3c10e2de
ravi2krishna <ravi2krishna@gmail.com>
2023-09-14 02:45:26 +0000

    create public rt association

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -55,4 +55,8 @@ resource "aws_route_table" "ecomm-pub-rt" {
   }
 }
 
-
+# Public Route Table Association
+resource "aws_route_table_association" "ecomm-pub-asc" {
+  subnet_id      = aws_subnet.ecomm-pub-sn.id
+  route_table_id = aws_route_table.ecomm-pub-rt.id
+}
